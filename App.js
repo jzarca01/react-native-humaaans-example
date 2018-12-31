28ed4bec610acb1654a090343e8a62c6a10a4db4
Jérémie Zarca <jeremie.zarca@gmail.com>
2019-01-01 00:23:48 +0100

    first release

--- App.js
+++ App.js
@@ -1,257 +1,73 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import {
-  Sitting1,
-  Sitting2,
-  Sitting3,
-  Sitting4,
-  Sitting5,
-  Sitting6,
-  Sitting7,
-  Sitting8,
-  Standing1,
-  Standing2,
-  Standing3,
-  Standing4,
-  Standing5,
-  Standing6,
-  Standing7,
-  Standing8
-} from 'humaaans-native';
+import tinycolor from 'tinycolor2';
+
+import { elements } from './elements';
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      count: 1
+      count: 1,
+      colors: this.createRandomColors()
     };
     this.changeHuman = this.changeHuman.bind(this);
+    this.createRandomColors = this.createRandomColors.bind(this);
+    this.renderRandomHuman = this.renderRandomHuman.bind(this);
   }
 
   changeHuman() {
     this.setState({
-      count: (this.state.count + 1) % 15
+      count: this.state.count + 1,
+      colors: this.createRandomColors()
     });
   }
 
+  renderRandomHuman() {
+    const positionArray = [
+      {
+        name: 'Standing',
+        modulo: 24
+      },
+      {
+        name: 'Sitting',
+        modulo: 8
+      }
+    ];
+    const { count, colors } = this.state;
+    return React.createElement(
+      elements[
+        `${positionArray[count % 2].name}${(count %
+          positionArray[count % 2].modulo) +
+          1}`
+      ],
+      {
+        hairColor: colors[0],
+        skinColor: colors[1],
+        shoeColor: colors[2],
+        pantColor: colors[3],
+        shirtColor: colors[4],
+        coatColor: colors[5],
+        objectColor: colors[6],
+        wheelchairColor: colors[7],
+        hatColor: colors[8]
+      }
+    );
+  }
+
+  createRandomColors() {
+    const randomColor = tinycolor.random();
+    const colors = randomColor.monochromatic((results = 10));
+    return colors.map(t => t.toHexString());
+  }
+
   render() {
-    const { count } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React-native Humaaans !</Text>
+        <Text style={styles.welcome}>Humaaans-Native Demo</Text>
         <TouchableOpacity onPress={this.changeHuman}>
-          {count === 0 && (
-            <Sitting1
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 1 && (
-            <Sitting2
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 2 && (
-            <Sitting3
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 3 && (
-            <Sitting4
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 4 && (
-            <Sitting5
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 5 && (
-            <Sitting6
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 6 && (
-            <Sitting7
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 7 && (
-            <Sitting8
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 8 && (
-            <Standing1
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 9 && (
-            <Standing2
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 10 && (
-            <Standing3
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 11 && (
-            <Standing4
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 12 && (
-            <Standing5
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 13 && (
-            <Standing6
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 14 && (
-            <Standing7
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
-          {count === 15 && (
-            <Standing8
-              hairColor="#000000"
-              skinColor="#ede0d4"
-              shoeColor="#0000EE"
-              pantColor="#FF44EA"
-              shirtColor="#FFAB11"
-              coatColor="#FF0E11"
-              objectColor="#FFA011"
-              wheelchairColor="#FF0000"
-              hatColor="#EEAABB"
-            />
-          )}
+          {this.renderRandomHuman()}
         </TouchableOpacity>
-        <Text style={styles.instructions}>
-          Don't forget to install react-native-svg too
-        </Text>
       </View>
     );
   }
@@ -268,10 +84,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
   }
 });
